b779bf0f0597917e5321be5e36d808400bbaf03c
golipathrakib <rakib.488125@gmail.com>
2020-04-09 01:47:17 +0600

    phplearning

---
+++ webdevelopment/variable.php
@@ -0,0 +1,16 @@
+<?php
+$name = "Rakibul Islam.";
+$age = "you're 28 years, aren't you?";
+$address = "Are you line in Shabgram, Bogra";
+
+$work = " An employee of Bd Themes, Banladesh.";
+$enjoy ="There is very much enjoyment in my work.";
+$colleague ="Colleagues, all are very cooperative.";
+$quesiton ="Are you happy with your boss?";
+/*echo 'Hello '.$name." ".$age;
+echo "\n";
+echo $age;
+echo "\n";
+echo $address;*/
+echo "Hello {$name}\n{$age}";
+
