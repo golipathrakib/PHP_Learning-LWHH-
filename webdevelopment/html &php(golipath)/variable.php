cc822ae9eab8c99e62762c985c2e1fb2d25cf082
golipathrakib <rakib.488125@gmail.com>
2020-04-10 03:54:55 +0600

    variable details

--- webdevelopment/html &php(golipath)/variable.php
+++ webdevelopment/html &php(golipath)/variable.php
@@ -1,2 +1,14 @@
 <?php
-echo"Hello World";
+$name = "Bangladesh";
+$age = 55;
+$word = "age";
+$question = "How are you?";
+
+echo $word ."<br/><br/> ";
+echo $$word ."<br/><br/>";
+echo $word ."<br/><br/>".$$word."<br/>";
+echo "Hello ".$name."<br/>";
+echo 'Hello $name' ."<br/>";
+echo "Hello $name .$question" ."<br/>";
+echo"Hello {$name}, {$question}";
+    
