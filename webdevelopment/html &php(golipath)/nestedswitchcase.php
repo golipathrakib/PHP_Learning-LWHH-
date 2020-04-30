1c4204370fe4568bcfc9755c670a6bdbc3b6b1d1
golipathrakib <rakib.488125@gmail.com>
2020-05-01 02:41:02 +0600

    nested switchcase

---
+++ webdevelopment/html &php(golipath)/nestedswitchcase.php
@@ -0,0 +1,25 @@
+<?php 
+    $n = 11;
+    $r = $n % 2;
+    switch ($r){
+      case 0:
+        switch($n){
+          case $n > 0:
+            echo "$n is a positive even number";
+            break;
+          case $n < 0:
+            echo "$n is a negative even number";
+            break;
+        }
+        break;
+        default;
+        switch($n){
+          case $n > 0:
+            echo "$n is a positive odd number";
+            break;
+          case $n < 0:
+            echo "$n is a negative odd number";
+            break;
+        }
+    }
+    ?>
