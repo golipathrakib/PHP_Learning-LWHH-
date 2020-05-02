4d7cbbee5e545ad335687adaf1e236189c735632
golipathrakib <rakib.488125@gmail.com>
2020-05-03 02:17:10 +0600

    switch_interest_2nd

--- webdevelopment/html &php(golipath)/switch_interest.php
+++ webdevelopment/html &php(golipath)/switch_interest.php
@@ -1,5 +1,5 @@
 <?php 
-    $string = "8balls";
+    /*$string = "8balls";
     switch($string){
       case"9balls";
         echo "Nine Ball";
@@ -10,5 +10,19 @@
       case "8balls";
         echo "Eight Balls";
         break;
+    }*/
+
+    $ball = "Rakibul";
+    switch($ball){
+      case(string)"9Rakibul";
+        echo "Nine Rakibul";
+        break;
+      case (string)8:
+        echo "8";
+        break;
+      case (string)"Rakibul";
+        echo "Eight Rakibul";
+        break;
     }
+
     ?> 
